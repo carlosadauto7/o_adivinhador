210978fb456b4ec59addc20a5477a62886bb9fb7
Carlos Adauto de Sá Batista <carlosadauto7oficial@gmail.com>
2025-10-31 15:31:24 -0300

    file: Novas funcionalidades de verificadores

--- pag1.c
+++ pag1.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-#include <locale.h> //
 
 int main() {
-    setlocale(LC_ALL, "Portuguese");
 
     printf("********************\n");
     printf("Jogo de adivinhação!\n");
@@ -13,6 +11,20 @@ int main() {
     int chute;
     printf("Digite o valor do seu chute: ");
     scanf("%d", &chute);
-    printf("O seu chute foi %d e o número secreto é: %d" , chute, numero_secreto);
 
+    int acertou = (chute == numero_secreto);
+
+    if(acertou) {
+        printf("Parabéns, você acaba de acertar o número. \n");
+    }
+    else {
+        int maior = (chute > numero_secreto); 
+
+        if (maior) {
+            printf("O seu número é maior do que o número secreto. \n");
+        }
+        else {
+            printf("O seu número é menor do que o número secreto. \n");
+        }
+    }
 }
