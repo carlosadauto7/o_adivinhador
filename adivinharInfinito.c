8cd0e8937b06c07107be1d53f8694b52692f4eac
Carlos Adauto <carlosadauto7oficial@gmail.com>
2025-11-25 16:27:00 -0300

    file(Pontução no jogo): Adicionei um sistema que contabiliza os pontos do jogo por meio de uma regra específica, bem como gerei um número randônico para o número secreto

--- adivinharInfinito.c
+++ adivinharInfinito.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main() {
+int main() 
+{
     // informações do cabeçalho do jogo 
     printf("********************\n");
     printf("Jogo de adivinhação!\n");
     printf("*******************\n");
 
     // informações das configurações do jogo
-    int numeroSecreto = 42;
+    int segundos = time(0);
+    srand(segundos);
+    int numero_grande = rand();
+
+    int numeroSecreto = numero_grande % 100;
     int ganhou = 0;  // Verificação do jogo em booleano
     int tentativasDoJogador = 0;
 
+    double pontos_jogo = 1000; 
 
-    // loop de infinito do jogo  
+
+    // loop infinito do jogo  
     while ( ganhou == 0 ) {
         
         int chute;
 
         printf("Tentativa : %d  \n" , tentativasDoJogador + 1 );
-        printf("Digite o valor edo seu chute: \n");
+        printf("Digite o valor do seu chute: \n");
         scanf("%d", &chute);
 
         if ( chute < 0) {
@@ -46,9 +55,16 @@ int main() {
 
         tentativasDoJogador += 1; 
 
+        float pontos_perdidos = abs(chute - numeroSecreto) / (float)2;
+        pontos_jogo -= pontos_perdidos;
+
+
+
+
     }
-    printf("Fim de jogo. Obrigado por jogar aqui.\n");
-    printf("Você acertou em %d tentativas" , tentativasDoJogador);
+    printf("Fim de jogo. Obrigado por jogar!! .\n");
+    printf("Você acertou em %d tentativas \n" , tentativasDoJogador);
+    printf("Você fez %.2f pontos, PARABÉNS!! \n" , pontos_jogo);
 
     return 0;
 }
